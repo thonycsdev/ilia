ec6c65bd20bebe3824ecbaded8a228098a52f0c1
Anthony Silva <thonycout@gmail.com>
2023-09-29 14:40:49 -0300

    repositories

--- ilia-api/Services/Interfaces/Repositories/IBaseRepository.cs
+++ ilia-api/Services/Interfaces/Repositories/IBaseRepository.cs
@@ -9,7 +9,7 @@ namespace Services.Interfaces.Repositories
     public interface IBaseRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>>? expression);
-        Task<T> GetSingleOrDefault(Expression<Func<T, bool>>? expression);
+        Task<T> GetSingleOrDefault(Expression<Func<T, bool>> expression);
         Task<T> Create(T TEntity);
         Task<T> Delete(T TEntity);
         Task<T> Update(T TEntity);
---
+++ ilia-api/Services/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+using Services.Entities;
+
+namespace Services.Interfaces.Repositories
+{
+    public interface IOrderRepository : IBaseRepository<Order>
+    {
+
+    }
+}
