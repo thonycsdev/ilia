0ffe11d42be011d3e4088271553a3eebe15fbd73
Anthony Silva <thonycout@gmail.com>
2023-09-29 10:26:40 -0300

    initial folder config. EF Core with SQLite

---
+++ ilia-api/Infra.Data/Context/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Context
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public string DbPath { get; }
+
+        public DatabaseContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "ilia.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
