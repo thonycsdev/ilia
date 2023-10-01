ef1961d928f67638a9464b02a1d74c91ec6c7a3e
Anthony Silva <thonycout@gmail.com>
2023-10-01 19:04:27 -0300

    delete loading and pop-up

--- ilia/src/Components/Costumers/CostumerProfile.tsx
+++ ilia/src/Components/Costumers/CostumerProfile.tsx
@@ -3,6 +3,7 @@ import { Costumer } from "@/models/costumer";
 import React, { useContext, useState } from "react";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 import { CostumerContext } from "@/contexts/costumerContext";
+import Toast from "../Toast/Toast";
 
 type CostumerProfileProps = {
 	costumer: Costumer;
@@ -11,15 +12,19 @@ type CostumerProfileProps = {
 function CostumerProfile(props: CostumerProfileProps) {
 	const { costumer } = props;
 	const [isLoading, setIsLoading] = useState(false);
+	const { successToast } = Toast();
 	const { deleteCostumer } = useContext(CostumerContext);
 	const handleOnClickDelete = () => {
 		setIsLoading(true);
-		deleteCostumer(costumer.id);
-		setIsLoading(false);
+		setTimeout(() => {
+			deleteCostumer(costumer.id);
+			setIsLoading(false);
+			successToast("Costumer Deleted");
+		}, 500);
 	};
 	return (
 		<div className="w-screen h-screen flex justify-center items-center pb-20">
-			<div className="w-4/6 h-3/4 flex flex-col items-center bg-slate-100 rounded-lg shadow-lg p-4">
+			<div className="w-4/6 h-full overflow-auto flex flex-col items-center bg-slate-100 rounded-lg shadow-lg p-4">
 				<div className="p-8 font-bold text-2xl">Costumer Infos</div>
 				<div className=" flex gap-7 appearance-none w-2/4 bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white">
 					<div className="font-bold">Nome:</div>
