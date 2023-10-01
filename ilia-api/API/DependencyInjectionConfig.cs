41719df2179a438dcb1f951563b3276fd29460e1
Anthony Silva <thonycout@gmail.com>
2023-09-30 22:32:07 -0300

    cross cutting DI

---
+++ ilia-api/API/DependencyInjectionConfig.cs
@@ -0,0 +1,18 @@
+﻿using Infra.Data.Repositories;
+using Services.Interfaces.Repositories;
+using Services.Interfaces.Services;
+using Services.Services;
+
+namespace ilia_api
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<ICustomerService, CustomerServices>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderService, OrderService>();
+        }
+    }
+}
